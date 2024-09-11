5c24aaecf8775828cddfe34d2e4d546a17b547c5
Srikanth-vepuri <srikanthvepuri@lyrostech.com>
2024-09-11 15:14:47 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
-import {BrowserRouter,Link,NavLink,Route,Routes} from 'react-router-dom'
-import AdminLoginPageFunction from './Ticket-Raising-Application/AdminLoginPageFunction.js';
-import AdminDashboard from './Ticket-Raising-Application/AdminDashBoard.js';
+import {BrowserRouter,Link,NavLink,Route,Routes} from 'react-router-dom';
+import { useState } from "react";
 import TicketsFunction from './Ticket-Raising-Application/TicketsFunction.js';
+import AdminLoginPageFunction from './Ticket-Raising-Application/AdminLoginPageFunction.js';
 import SignInPageFunction from './Ticket-Raising-Application/SignInPageFunction.js';
-import AdminDashBoardInOrder from './Ticket-Raising-Application/AdminDashBoardInOrder.js';
-// import './Ticket-Raising-Application/Styles/AdminDashboard.css';
-// import './Ticket-Raising-Application/Styles/AdminLoginClass.css';
-// import './Ticket-Raising-Application/Styles/AdminDashBoardInOrder.css';
-// import './Ticket-Raising-Application/Styles/ClosedTickets.css';
-// import './Ticket-Raising-Application/Styles/OpenedTickets.css';
-// import './Ticket-Raising-Application/Styles/SignInPage.css';
-// import './Ticket-Raising-Application/Styles/TicketsPageClass.css';
+import LoginPageFunction from './Ticket-Raising-Application/LoginPageFunction.js';
+import UserSignInPageFunction from './Ticket-Raising-Application/UserSignInPageFunction.js';
+import UserTicketFunction from './Ticket-Raising-Application/UserTicketFunction.js';
+import ViewTickets from './Ticket-Raising-Application/ViewTickets.js';
+import AdminDashBoardFunction from './Ticket-Raising-Application/AdminDashBoardFunction.js';
+import RiseTicket from './Ticket-Raising-Application/RiseTicket.js';
+import AdminDashBoardInOrderFunction from './Ticket-Raising-Application/AdminDashBoardInOrderFunction.js';
+import UserLoginPageFunction from './Ticket-Raising-Application/UserLoginPageFunction.js';
+import AuthContext from './Ticket-Raising-Application/AuthContext.js';
+
 function App(){
+  const [globalUsername,setUsername]=useState("")
   return(
     <>
-       { <BrowserRouter>
-       <Link to="/"></Link>
-       <Link to="/tickets"></Link>
-       <Link to="/signInPage"></Link>
-       <Link to="/adminDashboard"></Link>
-       <Link to="adminDashboardInOrder"></Link>
+       <BrowserRouter>
+       <AuthContext.Provider value={{globalUsername:globalUsername,setUsername:setUsername}}>
        <Routes>
-        <Route path="/" element={<AdminLoginPageFunction></AdminLoginPageFunction>}></Route>
+        <Route path="/" element={<LoginPageFunction></LoginPageFunction>}></Route>
+        <Route path="/adminLogin" element={<AdminLoginPageFunction></AdminLoginPageFunction>}></Route>
+        <Route path="/adminSignInPage" element={<SignInPageFunction></SignInPageFunction>}></Route>
+        <Route path="/userLogin" element={<UserLoginPageFunction></UserLoginPageFunction>}></Route>
+        <Route path="/userSignIn" element={<UserSignInPageFunction></UserSignInPageFunction>}></Route>
+        <Route path="/adminDashboard" element={<AdminDashBoardFunction></AdminDashBoardFunction>}></Route>
+        <Route path="/adminDashboardInOrder" element={<AdminDashBoardInOrderFunction></AdminDashBoardInOrderFunction>}></Route>
         <Route path="/tickets" element={<TicketsFunction></TicketsFunction>}></Route>
-        <Route path="signInPage" element={<SignInPageFunction></SignInPageFunction>}></Route>
-        <Route path="/adminDashboard" element={<AdminDashboard></AdminDashboard>}></Route>
-        <Route path="adminDashboardInOrder" element={<AdminDashBoardInOrder></AdminDashBoardInOrder>}></Route>
+        <Route path="/userTicket" element={<UserTicketFunction></UserTicketFunction>}></Route>
+        <Route path="/viewTicket" element={<ViewTickets></ViewTickets>}></Route>
+        <Route path="/riseTicket" element={<RiseTicket></RiseTicket>}></Route>
         </Routes>
-        </BrowserRouter> }
-
+        </AuthContext.Provider>
+        </BrowserRouter>
     </>
   )
 }
-export default App
+export default App;
